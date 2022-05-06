602efce6d2e1aebec1d13af6dba0f1790fa5cde0
Washington <wluizs245@gmail.com>
2022-05-06 10:11:26 -0300

    atualização atividade 1 React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import './App.css';
+import ListaUsuarios from './components/ListaUsuarios';
+// import ListaUserTest from './components/ListaUserTest';
+
+console.log('name');
+function App() {
+  return (
+    <div className="App">
+      <ListaUsuarios />
+    </div>
+  );
+}
+
+export default App;
